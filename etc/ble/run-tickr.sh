88c93b523a01a6492ff3a10ae19c727f072ce6c5
Clark Ritchie <clarkritchie@gmail.com>
2021-12-18 10:05:03 -0800

    messing around

--- etc/ble/run-tickr.sh
+++ etc/ble/run-tickr.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Determine the MAC of your decide with: sudo hcitool lescan
 MAC=${1:-df:7d:9c:da:1b:c5}
-./example.py ${MAC}
+./tickr-example.py ${MAC}
--- etc/ble/tickr-example.py
+++ etc/ble/tickr-example.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
 
+# this is a very simple example to pull heart rate data from a Wahoo TICKR
+# given a MAC address
+# connects to the device
+# print out what it can do
+# subscribe to the heart rate
+# receive data and print it out
+
 import sys
-from bluepy import btle
-import struct
-import binascii
+from bluepy import btle, BTLEException
 
-MAX=5
+# maximum number of subscription polls
+MAX=10
 
-class MyDelegate(btle.DefaultDelegate):
+class TICKRDelegate(btle.DefaultDelegate):
     def __init__(self):
         btle.DefaultDelegate.__init__(self)
 
     def handleNotification(self, cHandle, data):
         print("A notification was received: %s" % data)
-        print("heart rate is: %i" % int(data[1]))
+        # TICKR heart rate data is the second item
+        if len(data) >= 1:
+            print("Heart rate is: %i" % int(data[1]))
 
 deviceAddress=str(sys.argv[1])
 print("Connecting to: %s" % deviceAddress)
@@ -31,11 +39,12 @@ for service in peripheral.getServices():
      # Returns List of Characteristic objects, each represents a short data item which can be read or written
     for characteristic in service.getCharacteristics():
         print("Characteristic: %s" % str(characteristic))
-        print("UUID for this characteristic: %s" % characteristic.uuid)
-        print("properties as string: %s"  % characteristic.propertiesToString())
+        print("Characteristic's UUID: %s" % characteristic.uuid)
+        print("Characteristic's properties: %s"  % characteristic.propertiesToString())
         if "NOTIFY" in characteristic.propertiesToString():
-            print("establishing subscription")
+            print("Establishing subscription ")
             try:
+                # toggle the bit that allows us to establish a subscription
                 setup_data = b"\x01\x00"
                 notify_handle = characteristic.getHandle()
                 peripheral.writeCharacteristic(notify_handle+1, setup_data, withResponse=True)
@@ -45,21 +54,19 @@ for service in peripheral.getServices():
         if characteristic.supportsRead():
             try:
                 characteristicValue = characteristic.read()
-                print("(little) value as int: %i" % int.from_bytes(characteristicValue, "little"))
-                print("   (big) value as int: %i" % int.from_bytes(characteristicValue, "big"))
-                print("      value as string: %s" % characteristicValue.decode())
+                print("Characteristic value: %s" % characteristicValue.decode())
             except UnicodeError:
                 print("Characteristic is not UTF-8")
         print("\n")
 
-
-peripheral.withDelegate(MyDelegate())
+peripheral.withDelegate(TICKRDelegate())
 for i in range(MAX):
     if peripheral.waitForNotifications(1.0):
         continue
-    if i == MAX:
+    if i >= MAX:
         sys.exit("Exiting...")
-    print("Waiting...")
+    else:
+        print("Waiting...")
 
 print("Disconnecting from: %s" % deviceAddress)
 peripheral.disconnect()
